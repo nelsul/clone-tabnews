6f9784f373ac7171104ca2dbeeb62aa5cb62644f
Nelson <82171194+nelsul@users.noreply.github.com>
2025-03-10 05:22:35 +0000

    chore: create `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const dbStatus = await database.query("SELECT 8 + 8 AS sum");
   response.status(200).json({
     status: "Working fine!",
+    dbStatus: dbStatus.rows[0],
   });
 }
 
